30d8b3199fa557705af7206b201c551fa005d3fc
Nicolas <nicolasalmeida321@gmail.com>
2025-02-07 09:59:41 -0300

    Add React-Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 
+
 function App() {
-  
+
 }
 
 export default App
