d7943c470c27bad168e6b1db67cc028dd09586e1
Nicolas <nicolasalmeida321@gmail.com>
2025-02-07 10:25:14 -0300

    Add 'Layout' and 'Pages'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 import './App.css'
 
+import Layout from './components/Layout'
+
+/* pages */
+import Home from './pages/Home'
 
 function App() {
-
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Layout>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Meu Site</h1>
+    </header>
+  )
+}
+
+export default Header
